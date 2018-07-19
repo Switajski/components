4554a93d4c86fef006bb2adcc7fe7513a497ec87
Marek <marek@switajski.de>
2018-07-19 09:37:23 +0200

    added create-react-app and a card component

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Card = styled.div`
+background-color: #fff;
+border-radius: .125em;
+-webkit-box-shadow: 0 2px 3px rgba(0,0,0,.075), 0 0 0 1px rgba(0,0,0,.1);
+box-shadow: 0 2px 3px rgba(0,0,0,.075), 0 0 0 1px rgba(0,0,0,.1);
+margin-bottom: 1em;
+padding: 1em;`
+
+export default Card
